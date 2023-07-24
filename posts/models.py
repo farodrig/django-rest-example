4fd367b647e80cbb38ae46b9adaed1305cf1cac4
Felipe Rodríguez <farodrig@Felipes-MacBook-Pro.local>
2023-07-24 16:23:02 -0700

    Fix users posts and comments

--- posts/models.py
+++ posts/models.py
@@ -4,12 +4,12 @@ from model_utils.models import TimeStampedModel
 
 User = get_user_model()
 class Post(TimeStampedModel):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=250)
     body = models.TextField()
 
 
 class Comment(TimeStampedModel):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     body = models.TextField()
--- users/serializers.py
+++ users/serializers.py
@@ -2,17 +2,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from rest_framework import serializers
 
+from posts.serializers import PostSerializer
+
 User = get_user_model()
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    posts = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='post-detail'
-    )
-
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups', 'created', 'modified', 'posts']
+        fields = ['url', 'username', 'email', 'groups', 'created', 'modified', 'posts', 'comments']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
